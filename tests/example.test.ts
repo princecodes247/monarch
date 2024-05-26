6cec11c4eba000da7c0cc0c85e9a83c7c71feb91
Eric Afes <eriicafes@gmail.com>
2024-05-26 17:38:42 +0100

    Add tests

---
+++ tests/example.test.ts
@@ -0,0 +1,47 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { createSchema, monarch, number, string } from "../src";
+
+describe("Monarch API", () => {
+  beforeAll(() => {
+    monarch.connect("mongodb://localhost:27017/monarch-test");
+  });
+
+  it("inserts and finds", async () => {
+    const UserSchema = createSchema("users", {
+      name: string().nullable(),
+      email: string().optional().lowercase(),
+      age: number().optional().default(10),
+    });
+
+    const newUser = await UserSchema.insert({
+      email: "anon@gmail.com",
+      name: "anon",
+      age: 0,
+    });
+
+    expect(newUser).not.toBe(null);
+    expect(newUser).not.toStrictEqual({
+      email: "anon@gmail.com",
+      name: "anon",
+      age: 0,
+    });
+
+    const users = await UserSchema.find()
+      .where({ age: { $lte: 18 } })
+      .select({
+        age: 1,
+        _id: 0,
+      })
+      .limit(1)
+      .exec();
+
+    expect(users.length).toBeGreaterThan(1);
+
+    const existingUser = users[0];
+    expect(existingUser).toStrictEqual({
+      email: "anon@gmail.com",
+      name: "anon",
+      age: 0,
+    });
+  });
+});
