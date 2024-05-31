8b2b26f0b583faf6bf54f64a815d6761611fea87
Prince Codes <princecodes247@gmail.com>
2024-05-31 18:04:41 +0100

    write base type and test

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { MonarchError } from "./errors";
 export { InferSchemaInput, InferSchemaOutput, createSchema } from "./schema";
 export { boolean } from "./types/boolean";
 export { date, dateString } from "./types/date";
+export { literal } from "./types/literal";
 export { number } from "./types/number";
 export { string } from "./types/string";
 export {
