b4e0b307f9ed72950ddd3942b9789fa259e6ad77
Prince Codes <princecodes247@gmail.com>
2024-05-31 18:03:50 +0100

    update date test

--- tests/date.test.ts
+++ tests/date.test.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { beforeAll, describe, expect, it } from "vitest";
-import { createDatabase, createSchema, date } from "../src";
+import { createDatabase, createSchema, date, dateString } from "../src";
 
 const mongod = await MongoMemoryServer.create();
 
@@ -13,7 +13,7 @@ describe("test for date", () => {
     await client.connect();
   });
 
-  it("inserts and finds", async () => {
+  it("insert's date object and find's it", async () => {
     const UserSchema = createSchema("users", {
       currentDate: date(),
     });
@@ -46,6 +46,43 @@ describe("test for date", () => {
     expect(existingUser).toStrictEqual(
       expect.objectContaining({
         currentDate: markedDate,
+      })
+    );
+  });
+
+  it("insert's date string and find's it", async () => {
+    const UserSchema = createSchema("users", {
+      currentDate: dateString(),
+    });
+    const markedDate = new Date();
+    const { db, collections } = createDatabase(client, {
+      users: UserSchema,
+    });
+
+    // collections query builder
+    const newUser = await collections.users
+      .insert({
+        currentDate: markedDate,
+      })
+      .exec();
+    expect(newUser).not.toBe(null);
+    expect(newUser).toStrictEqual(
+      expect.objectContaining({
+        currentDate: markedDate.toISOString(),
+      })
+    );
+
+    // db query builder
+    const users = await db(UserSchema)
+      .find()
+      .where({ currentDate: markedDate.toISOString() })
+      .exec();
+    expect(users.length).toBeGreaterThanOrEqual(1);
+
+    const existingUser = users[0];
+    expect(existingUser).toStrictEqual(
+      expect.objectContaining({
+        currentDate: markedDate.toISOString(),
       })
     );
   });
